92ea3b3817427fc03df595ed4a5f863dd605099a
Garry201199 <72191123+Garry201199@users.noreply.github.com>
2023-04-01 15:28:44 +0530

    Revert "Commit with hero section in test"
    This reverts commit 80fc69a1b728c35c041bc73deb1b04eff39ab1af.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,16 +8,7 @@ module.exports = {
     fontFamily: {
       raleway : ['Raleway', 'sans-serif'],
     },
-    extend: {
-      screens: {
-        xs: "480px",
-        ss: "620px",
-        sm: "768px",
-        md: "1060px",
-        lg: "1200px",
-        xl: "1700px",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
